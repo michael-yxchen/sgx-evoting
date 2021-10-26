3eeed670eb18a02363570a06d0491668e7f8a0da
Ubuntu <rokvm@BTRvm.vfit4j4otgpehgezwm5gt21dkg.bx.internal.cloudapp.net>
2021-10-26 20:58:06 +0000

    election script

--- run_election.sh
+++ run_election.sh
@@ -43,6 +43,7 @@ Biden
 Trump' > ballot.txt
 echo "Ballot created for 3 candidates: Barrack, Joe, Donald.\n"
 
+# Admin invoking the Ecall to initialize the election
 ../app/app --init \
     --enclave-path `pwd`/../enclave/enclave.signed.so \
 		--sealedkey sealedkey.bin \
@@ -51,33 +52,86 @@ echo "Ballot created for 3 candidates: Barrack, Joe, Donald.\n"
 		--bulletin bulletin.txt \
 		--voter1 alice.pem \
 		--voter2 john.pem \
-		--voter3 justin.pem
+		--voter3 justin.pem \
+		--sealedelec sealedhelios_state.bin
 #open sealed key, initialize state counter to 1, save admin public key, save pks of eligible voters, save ballot, seal meta data, compute hash metadata(-counter)+PK, save election identifier
 
-echo 'placeholder' > open_command.txt
+
+
+# Admin invoking the Ecall to open the election
+echo '{ command: open }' > open_command.json
+echo -n "Signing command with admin key.."
+openssl dgst -sign secp256r1-key.pem -out Open.signature ../open_command.json
+echo ".done"
 
 ../app/app --open \
     --enclave-path `pwd`/../enclave/enclave.signed.so \
-		--adminsign open_command.txt
-#state counter+1
+		--adminsign Open.signature
 
-echo 'placeholder' > signed_encrypted_ballot.txt
-echo 'placeholder' > election.txt
+
+
+# Observing election hash from bulletin board and saving to seperate file
+head -c 32 bulletin.txt > election.hash
+
+# Actions Alice would take to cast her vote
+echo '{ vote: 00000010 }' > ballot.json
+./ballot_preparation_script ballot.json encrypted_ballot_alice.json
+echo -n "Signing ballot with alice's key.."
+openssl dgst -sign alice-key.pem -out EncBallotAlice.signature ../encrypted_ballot_alice.json
+echo ".done"
 
 ../app/app --cast \
     --enclave-path `pwd`/../enclave/enclave.signed.so \
-		--usersign signed_encrypted_ballot.txt \
-		--electionhash election.txt
-#open sealed metadata if counter == 2, check signed ballot with eligible pks, add pk, ciphertext, and hash to sealed ballots, return hash
+		--usersign EncBallotAlice.signature \
+		--electionhash election.hash
 
-echo 'placeholder' > close_command.txt
+
+
+# Actions Justin would take to cast her vote
+echo '{ vote: 00000100 }' > ballot.json
+./ballot_preparation_script ballot.json encrypted_ballot_justin.json
+echo -n "Signing ballot with justin's key.."
+openssl dgst -sign justin-key.pem -out EncBallotJustin.signature ../encrypted_ballot_justin.json
+echo ".done"
+
+../app/app --cast \
+    --enclave-path `pwd`/../enclave/enclave.signed.so \
+		--usersign EncBallotJustin.signature \
+		--electionhash election.hash
+		
+		
+		
+# Actions John would take to cast her vote		
+echo '{ vote: 00000100 }' > ballot.json
+./ballot_preparation_script ballot.json encrypted_ballot_john.json
+echo -n "Signing ballot with john's key.."
+openssl dgst -sign john-key.pem -out EncBallotJohn.signature ../encrypted_ballot_john.json
+echo ".done"
+
+../app/app --cast \
+    --enclave-path `pwd`/../enclave/enclave.signed.so \
+		--usersign EncBallotJohn.signature \
+		--electionhash election.hash
+
+
+
+# Admin invoking the Ecall to close the election
+echo '{ command: close }' > close_command.json
+echo -n "Signing command with admin key.."
+openssl dgst -sign secp256r1-key.pem -out Close.signature ../close_command.json
+echo ".done"
 
 ../app/app --close \
     --enclave-path `pwd`/../enclave/enclave.signed.so \
-		--adminsign close_command.txt
-#open sealed metadata counter+1
+		--adminsign Close.signature
+		
+		
 
-echo 'placeholder' > tally_command.txt
+# Admin invoking the Ecall to tally the election
+echo '{ command: tally }' > open_command.json
+echo -n "Signing command with admin key.."
+openssl dgst -sign secp256r1-key.pem -out Open.signature ../open_command.json
+echo ".done"
 
 ../app/app --tally \
     --enclave-path `pwd`/../enclave/enclave.signed.so \
